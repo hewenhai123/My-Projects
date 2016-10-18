d3e01eabc1c07639cfb14671aa1679cf517aed3d
hewenhai123 <1194630180@qq.com>
2016-10-19 00:30:40 +0800

    09

---
+++ h5/js/index.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+
+//function a() {
+//	var swiper = new Swiper(".swiper-container", {
+//		loop: true,
+//		direction: 'vertical',
+//		onInit: function(swiper) {
+//			swiperAnimateCache(swiper);
+//			swiperAnimate(swiper);
+//
+//		},
+//		onSlideChangeEnd: function(swiper) {
+//			swiperAnimate(swiper);
+//
+//		}
+//
+//	});
+//}
+//
+//a()
+
+// 音乐加载
+//~function () {
+//
+//  var music = document.querySelector("#sound"), audioF = document.querySelector(".audioFir");
+//  console.log(music, audioF);
+//  window.setTimeout(function () {
+//      audioF.play();
+//      audioF.addEventListener("canplay", function () {
+//          music.style.display = "block";
+//          music.className = "sound soundMove";
+//      }, false)
+//  }, 500);
+//  music.addEventListener("click", function () {
+//      if (audioF.paused) {
+//          audioF.play();
+//          music.style.webkitAnimationPlayState = "running";
+//          return;
+//      }
+//      audioF.pause();
+//      music.style.webkitAnimationPlayState = "paused";
+//  }, false);
+//}();
+
+
